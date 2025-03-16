03eef5460e02c394557e7c23be7123f15557a6f6
Garry Amasia <amasiagarry23@gmail.com>
2025-03-16 20:12:09 +1100

    adding rewrites vercel.json

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -13,12 +13,12 @@ const User = () => {
   // Cegah error jika `user` adalah `null`
   if (!user) return null;
   return (
-    <div className="w-[20%] absolute top-4 right-4 bg-stone-700 rounded-md z-[999]">
-      <div className="py-1 flex justify-evenly items-center text-sm text-white">
+    <div className="md:w-[205px] md:h-[40px] w-[20%] text-xs  absolute top-4 right-4 bg-slate-800 rounded-md z-[999]">
+      <div className="py-1 flex md:justify-evenly items-center text-white">
         <img
           src={user.avatar}
           alt={user.name}
-          className="w-[30px] h-[30px] rounded-full"
+          className="hidden md:block w-[30px] h-[30px] rounded-full"
         />
         <span>Hey, {user.name}</span>
         <button className="bg-red-500 px-2 rounded-md" onClick={handleClick}>
